3e59818fcbf4b0c30fab4a558e01aefd64c7ff13
nuyuy31 <hn6401847@gmail.com>
2024-12-17 14:03:15 +0700

    nambah logo

--- lk.php
+++ lk.php
@@ -36,11 +36,9 @@ include ('./layouts/header.php');
       </div>
 
       <div class="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-in">
-        <h2>SMK Bina Cendekia Cirebon</h2>
-        <p>Jadilah bagian dari SMK Bina Cendekia Cirebon, tempat di mana masa depanmu dimulai dengan pendidikan berkualitas, keterampilan nyata, dan peluang karir yang tak terbatas!.</p>
-        <div class="d-flex">
-          <a href="daftar.php" class="btn-get-started">Lihat Selengkapnya >></a>
-        </div>
+        <h2>Layanan Kesehatan</h2>
+        <p>Rekayasa Perangkat Lunak (RPL) adalah bidang yang fokus pada pengembangan dan pemeliharaan perangkat lunak berkualitas. Prosesnya meliputi analisis, desain, pengkodean, dan pengujian.
+         </div>
       </div>
     </div>
   </div>
@@ -60,6 +58,169 @@ include ('./layouts/header.php');
     </g>
   </svg>
 </section>
+
+ 
+ <section id="details" class="details section">
+      <div class="container">
+        <div class="row gy-4 align-items-center">
+          <div class="col-md-5 d-flex align-items-center" data-aos="zoom-out" data-aos-delay="100">
+            <img src="assets/img/logo/lk.png" class="img-fluid" alt="" style="width: 85%; height: 450px;">
+          </div>
+          <div class="col-md-7" data-aos="fade-up" data-aos-delay="100">
+            <h1>Layanan Kesehatan (LK) SMK Bina Cendekia Cirebon</h1>
+            <p>Layanan kesehatan merujuk pada berbagai jenis perawatan medis yang diberikan untuk menjaga atau meningkatkan kesehatan individu. Layanan ini meliputi pencegahan, diagnosis, pengobatan, serta pemulihan penyakit atau cedera. Biasanya, layanan kesehatan disediakan oleh tenaga medis profesional seperti dokter, perawat, dan tenaga medis lainnya di rumah sakit, klinik, atau fasilitas kesehatan lainnya. Selain itu, layanan kesehatan juga mencakup pelayanan kesehatan mental, gigi, dan obat-obatan, yang bertujuan untuk memenuhi kebutuhan kesehatan fisik dan mental masyarakat.</p>
+            <p>Layanan kesehatan memiliki peran penting dalam meningkatkan kualitas hidup dan produktivitas masyarakat. Akses yang baik terhadap layanan kesehatan dapat mengurangi angka kematian, mencegah penyebaran penyakit, dan membantu orang pulih dari kondisi medis. Namun, tantangan dalam layanan kesehatan, seperti keterbatasan akses, biaya, dan kualitas, masih perlu diatasi untuk memastikan setiap individu dapat memperoleh perawatan yang mereka butuhkan. Teknologi dan inovasi, seperti telemedicine dan sistem kesehatan digital, semakin mendukung efisiensi dan aksesibilitas layanan kesehatan.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="gallery" class="gallery section">
+
+<!-- Section Title -->
+<div class="container section-title" data-aos="fade-up">
+  <h2>Mengenai</h2>
+  <div><span>Layanan Kesehatan</span> <span class="description-title">di SMK Bina Cendekia Cirebon</span></div>
+</div><!-- End Section Title -->
+
+<div class="container" data-aos="fade-up" data-aos-delay="100">
+
+  <div class="row g-0">
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/rpl.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/rpl.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas RPL
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/rpl.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/rpl.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas RPL
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/rpl.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/rpl.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas RPL
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/rpl.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/rpl.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas RPL
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/rpl.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/rpl.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas RPL
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/lk.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/lk.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas LK
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/tbo.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/tbo.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas TBO
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/tabus.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/tabus.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas Tabus
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/sti.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/sti.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas STI
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/perpus.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/perpus.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas Perpustakaan
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/toilet.jpg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/toilet.jpg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas Toilet
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+
+    <div class="col-lg-3 col-md-4">
+      <div class="gallery-item">
+        <a href="assets/img/gallery/rpl.jpeg" class="glightbox" data-gallery="images-gallery">
+          <img src="assets/img/gallery/rpl.jpeg" class="img-fluid" alt="" style="width: 110%; height: 240px; object-fit: cover;">
+        </a>
+        <div class="caption" style="text-align: center; padding: 5px; background-color: rgba(0, 0, 0, 0.5); color: white; font-size: 12px;">
+          Fasilitas RPL
+        </div>
+      </div>
+    </div><!-- End Gallery Item -->
+
+  </div>
+
+</div>
+
+</section>
+  
 
 <footer id="footer" class="footer dark-background">
 
